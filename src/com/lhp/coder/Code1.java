1d179cc3ab4638376f653fddd46df3dfca587b6f
lhp <770284670@qq.com>
2019-10-20 20:37:46 +0800

    数组中的查找

--- src/com/lhp/coder/Code1.java
+++ src/com/lhp/coder/Code1.java
@@ -1,4 +1,31 @@
 package com.lhp.coder;
 
+/**
+ * int array[][]={{1,2,3},
+ * {4,5,6},
+ * {7,8.9}}
+ */
 public class Code1 {
+    public boolean Find(int target, int[][] array) {
+        if (array.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (target == array[i][j]) {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Code1 code1 = new Code1();
+        int target = 22;
+        int array[][] ={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(code1.Find(target,array));
+    }
 }
