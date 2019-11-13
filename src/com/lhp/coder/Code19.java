27ad9eb91b451bb6183628a78924aef124474afe
lihuaipeng3 <lihuaipeng3@jd.com>
2019-11-13 20:11:17 +0800

    数组半数

---
+++ src/com/lhp/coder/Code19.java
@@ -0,0 +1,34 @@
+package com.lhp.coder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @ClassName: Code19
+ * @Description:数组中出现次数超过一半的数字
+ * @Author: 李怀鹏
+ * @Company: JD-BJUT
+ * @Date： 2019/11/13 19:46
+ * @Version： V1.0
+ **/
+public class Code19 {
+    public int MoreThanHalfNum_Solution(int[] array) {
+        int len = array.length;
+        if (array == null || len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return array[0];
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i : array) {
+            set.add(i);
+        }
+        int len1 = set.size();
+        if (len1 > len / 2 + 1) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
