41bed4d18dc0d6b93a0b7320ec10aecb084cddd7
lihuaipeng3 <lihuaipeng3@jd.com>
2019-11-15 15:45:18 +0800

    数组中查找大于数组长度一半的值

--- src/com/lhp/coder/Code19.java
+++ src/com/lhp/coder/Code19.java
@@ -1,7 +1,8 @@
 package com.lhp.coder;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * @ClassName: Code19
@@ -20,15 +21,25 @@ public class Code19 {
         if (len == 1) {
             return array[0];
         }
-        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap();
         for (int i : array) {
-            set.add(i);
+            if (!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                map.put(i, map.get(i) + 1);
+            }
         }
-        int len1 = set.size();
-        if (len1 > len / 2 + 1) {
-            return 0;
+        for (int i : map.keySet()) {
+            if (map.get(i)>array.length/2){
+                return i;
+            }
         }
+        return 0;
+    }
 
-        return 222;
+    public static void main(String[] args) {
+        Code19 code19 = new Code19();
+        int[] arr = {};
+        System.out.println(code19.MoreThanHalfNum_Solution(arr));
     }
 }
