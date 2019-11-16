b0b6b79aedf8c234689ba12352a755186d4cb2be
lhp <770284670@qq.com>
2019-11-16 17:35:00 +0800

    lhp

---
+++ src/com/lhp/coder/lhp.java
@@ -0,0 +1,14 @@
+package com.lhp.coder;
+
+/**
+ * @ClassName: lhp
+ * @Description
+ * @Author: lhp
+ * @Date: 2019/11/16 17:33
+ * @Version: V1.0
+ **/
+public class lhp {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
